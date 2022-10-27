06181bf5bda819a9f9afe760d46943589c6fa773
arinapruzhinina <113105119+arinapruzhinina@users.noreply.github.com>
2022-10-27 11:43:54 +0300

    Delete homework_28_10.py

--- homework_28_10.py
+++
@@ -1,9 +0,0 @@
-from datetime import date, timedelta
-def captain_diary(notes, date):
-    a=open("diary.txt", "w")
-    b=timedelta(days=1)
-    for i in notes:
-        a.write(str(date)+ ': '+ i + '\n')
-        date+=b 
-    a.close()
-captain_diary(['Привет', 'Как дела?'], date(2022, 10, 23))
