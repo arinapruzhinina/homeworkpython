06e8891cc113c8b38a966380b2be53394ac9a3f9
arinapruzhinina <pruzininaa@mail.ru>
2022-10-27 11:44:54 +0300

    Домашнее задание 28.09

--- homework_28_10.py
+++ homework_28_10.py
@@ -1,9 +1,10 @@
 from datetime import date, timedelta
+
 def captain_diary(notes, date):
-    a=open("diary.txt", "w")
-    b=timedelta(days=1)
-    for i in notes:
-        a.write(str(date)+ ': '+ i + '\n')
-        date+=b 
-    a.close()
+    with open("diary.txt", "w") as f:
+        d = timedelta(days=1)
+        for i in notes:
+            f.write(str(date)+ ': '+ i + '\n')
+            date += d 
+
 captain_diary(['Привет', 'Как дела?'], date(2022, 10, 23))
